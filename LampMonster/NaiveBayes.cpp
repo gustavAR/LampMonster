78926da0a5df425497f6b88e3dc98a7cbbcbf8b5
Akodiat <joakimjo@swipnet.se>
2013-04-11 14:16:01 +0200

    Edited NaiceBayes to fit the refactoring and the updated header files

--- LampMonster/NaiveBayes.cpp
+++ LampMonster/NaiveBayes.cpp
@@ -1,32 +1,81 @@
 #include <iostream>
 #include "NaiveBayes.h"
+#include "BuildBagOfWords.h"
 
 #define VERSION_NUMBER 0.0
 #define DEFAULT_TRAIN_SIZE 10
 
 
 using namespace std;
-
-double NBC(double probOfClass, const map<string,int>& vocabulary, const vector<string>& words, double prior)
+class NaiveBayes : public Classifier
 {
+private:
+	std::vector<Category> categories;
+	int prior;
+
+	double NBC(double probOfClass, const map<string,int>& vocabulary, const vector<string>& words, double prior)
+	{
+		double product = 1;
+		int totNumWordsInVoc = 0;
+
+		for(auto i = vocabulary.cbegin(); i != vocabulary.cend(); i++) //Get the sum of all words in the vocabulary map
+			totNumWordsInVoc += i->second;
+
+		double nom = (totNumWordsInVoc + vocabulary.size()*prior);
+		for (string word : words) //calculate probalility for each word to belong to classification with the vocabulary
+		{
+			double denom = prior;
+			if (vocabulary.find(word) != vocabulary.end())
+				denom += vocabulary.at(word);
+			product *= (denom/nom);
+		}
+		return product*probOfClass;
+	}
+
+public:
+	NaiveBayes(CategoryPaths paths)
+	{
+		categories = vector<Category> (paths.size()); //TODO: Think over this...
+		int i = 0;
+		for(auto path = paths.cbegin(); path != paths.cend(); path++) //for each training path in paths
+			categories[i++] = *(new Category(path->first,BuildBagOfWords(path->second)));	//Build and fill a category with a bag of words
+	}
+
+	virtual string Classify(vector<string> words)
+	{
+		Category fittest = categories.begin;	//Set first category as default
+		int maxProb = 0;						//Set current maximum category probability to 0
+		for(Category c : categories) {			//For each category
+			int curProb = (NBC(1/categories.size, c.second, words, prior)); //Calculate probability for the words belonging to that category
+			if (curProb>maxProb) {				//If probability is higher that of the previous most probable (fittest) category
+				maxProb = curProb;				//Update maximums
+				fittest = c;
+			}	
+		}
+		return fittest.first;					//Returns the name of the most probable category
+	}
+	/*
+	double NBC(double probOfClass, const map<string,int>& vocabulary, const vector<string>& words, double prior)
+	{
 	double product = 1;
 	int totNumWordsInVoc = 0;
 
 	for(auto i = vocabulary.cbegin(); i != vocabulary.cend(); i++) //Get the sum of all words in the vocabulary map
-		totNumWordsInVoc += i->second;
+	totNumWordsInVoc += i->second;
 
 	double nom = (totNumWordsInVoc + vocabulary.size()*prior);
 	for (string word : words) //calculate probalility for each word to belong to classification with the vocabulary
 	{
-		double denom = prior;
-		if (vocabulary.find(word) != vocabulary.end())
-			denom += vocabulary.at(word);
-		product *= (denom/nom);
+	double denom = prior;
+	if (vocabulary.find(word) != vocabulary.end())
+	denom += vocabulary.at(word);
+	product *= (denom/nom);
 	}
 	return product*probOfClass;
-}
-void NaiveBayesTest(void)
-{
+	}
+
+	void NaiveBayesTest(void)
+	{
 	map<string,int> m;
 	m["Buuuh"] = 2;
 	m["Aaah"] = 1;
@@ -53,4 +102,5 @@ void NaiveBayesTest(void)
 	s[6]="Hehehaha";
 
 	cout << "Result: " << (NBC(0.5, m, s, 1) < NBC(0.5, m2, s, 1)? "yay!":"ops") <<endl;
-}
+	}
+	*/
